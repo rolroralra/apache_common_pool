b319b29af10ef36e549d91261487d42f5561c309
Shinyoung Kim <rolroralra@gmail.com>
2020-07-10 17:38:45 +0900

    Update MainTest.java

--- src/test/java/test/MainTest.java
+++ src/test/java/test/MainTest.java
@@ -1,8 +1,14 @@
 package test;
 
+import connection.ConnectionConfig;
+import lombok.SneakyThrows;
+import model.FTPClient;
 import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainTest {
+    static Logger LOGGER = LoggerFactory.getLogger(MainTest.class);
 
     @BeforeClass
     public static void beforeClass() {
@@ -26,7 +32,33 @@ public class MainTest {
 
     @Test()
     public void test() {
+        for (int i = 0; i < 20; i++) {
+            try {
+                new Thread(new Runnable() {
+                    @SneakyThrows
+                    public void run() {
+                        FTPClient ftpClient = ConnectionConfig.objectPool.borrowObject();
+                        ftpClient.download(null, null);
+                        ftpClient.upload(null, null);
+                        ConnectionConfig.objectPool.returnObject(ftpClient);
+                    }
+                }).start();
+            } catch (Exception e) {
+                LOGGER.error(e.toString());
+                for (StackTraceElement stackTraceElement : e.getStackTrace()) {
+                    LOGGER.error(stackTraceElement.toString());
+                }
+            }
+        }
 
+
+        while (Thread.activeCount() > 2) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
