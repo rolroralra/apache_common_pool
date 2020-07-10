d4e4b11b4b2af70cfbd8ee59e5a96daa5b286220
Shinyoung Kim <rolroralra@gmail.com>
2020-07-10 16:43:32 +0900

    Create FTPClientPool.java

---
+++ src/main/java/connection/FTPClientPool.java
@@ -0,0 +1,18 @@
+package connection;
+
+import model.FTPClient;
+import org.apache.commons.pool2.BasePooledObjectFactory;
+import org.apache.commons.pool2.PooledObject;
+import org.apache.commons.pool2.impl.DefaultPooledObject;
+
+public class FTPClientPool extends BasePooledObjectFactory<FTPClient> {
+    @Override
+    public FTPClient create() throws Exception {
+        return new FTPClient();
+    }
+
+    @Override
+    public PooledObject<FTPClient> wrap(FTPClient ftpClient) {
+        return new DefaultPooledObject<FTPClient>(ftpClient);
+    }
+}
